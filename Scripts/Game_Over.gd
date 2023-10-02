4a9be53dc299a15c4fe3bf03dec8658410c57679
ijzm <ignacio.zevallos@hotmail.com>
2023-10-01 22:21:31 -0500

    Added press space to restart

--- Scripts/Game_Over.gd
+++ Scripts/Game_Over.gd
@@ -9,3 +9,8 @@ func _on_play_again_pressed():
 
 func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://Levels/main_menu.tscn")
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_SPACE:
+			get_tree().change_scene_to_file("res://Levels/game.tscn")
